6c0f2779bd38a8670cad2fd837456655c76d4b06
Deep Sathwara <72172951+deepsathwara01@users.noreply.github.com>
2021-04-14 01:59:01 -0400

    Add files via upload

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Deep Sathwara on 2020-09-08.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
